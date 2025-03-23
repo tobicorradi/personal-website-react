553f144ad67b04e7b64aeebb23b6290a75d50ef4
tobicorradi <corraditobias@gmail.com>
2025-03-23 13:20:59 -0300

    absolute path added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@hooks": fileURLToPath(new URL("./src/hooks", import.meta.url)),
+      "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
+    },
+  },
 });
