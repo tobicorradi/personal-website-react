ab3d844113039854281d0e572620e2c6270b5da1
tobicorradi <corraditobias@gmail.com>
2025-03-23 12:39:12 -0300

    font added

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+import "./assets/fonts/Gilroy/gilroy-font.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
